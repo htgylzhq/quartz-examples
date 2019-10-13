c80b0d1b986dc7d8543ae5bd12212a2997267513
lzhq <htgylzhq@gmail.com>
2019-10-13 21:43:39 +0800

    Simple Triggers - Shows a dozen different ways of using Simple Triggers to schedule your jobs

---
+++ src/main/java/org/quartz/examples/example2/SimpleJob.java
@@ -0,0 +1,40 @@
+package org.quartz.examples.example2;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.JobKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+/**
+ * This is just a simple job that gets fired off many times by example 2
+ *
+ * @author Bill Kratzer
+ */
+public class SimpleJob implements Job {
+
+  private static final Logger logger = LoggerFactory.getLogger(SimpleJob.class);
+
+  /**
+   * Empty constructor for job initialization
+   */
+  public SimpleJob() {
+  }
+
+  /**
+   * Called by the <code>{@link org.quartz.Scheduler}</code> when a <code>{@link org.quartz.Trigger}
+   * </code> fires that is associated with the <code>Job</code>.
+   *
+   * @throws JobExecutionException if there is an exception while executing the job.
+   */
+  public void execute(JobExecutionContext context) throws JobExecutionException {
+
+    // This job simply prints out its job name and the
+    // date and time that it is running
+    JobKey jobKey = context.getJobDetail().getKey();
+    logger.info("SimpleJob says: " + jobKey + " executing at " + new Date());
+  }
+}
